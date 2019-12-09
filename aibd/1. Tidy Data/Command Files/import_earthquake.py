8814f3ec4e306e2da58905f5d0249aa6caa201e9
bloodi11 <jds.dabrowa@gmail.com>
2019-12-09 14:22:11 +0100

    Add files via upload

--- aibd/1. Tidy Data/Command Files/import_earthquake.py
+++ aibd/1. Tidy Data/Command Files/import_earthquake.py
@@ -14,18 +14,6 @@ new_data.insert(2, "One Big happen?", answer, True)
 
 rank = new_data.groupby(['Age', 'Gender', 'One Big happen?'])
 
-
-#new_data.insert(0, "Count", count, True)
-
-# final_data = pd.DataFrame()
-
-# final_data.insert(0, "Age", age, False)
-# final_data.insert(1, "Gender", gender, False)
-# final_data.insert(2, "One Big happen?", answer, False)
-
 print(new_data)
 
-# age = data.set_index("Age", drop = False)
-# gender = data.set_index("What is your gender?", drop = False)
-# answer = data.set_index("Do you think the \"Big One\" will occur in your lifetime?", drop = False)
-
+new_data.to_csv(index=False)
